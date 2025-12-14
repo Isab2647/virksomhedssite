beb15d4fca90bff36f9c959d1598d1378b5b9db0
Emma Persson <empe0009@stud.ek.dk>
2025-12-14 19:00:32 +0100

    burger change

--- JAVA/burger.js
+++ JAVA/burger.js
@@ -1,8 +1,15 @@
-const burger = document.querySelector(".burger");
-const nav = document.querySelector("nav");
+// const burger = document.querySelector(".burger");
+// const nav = document.querySelector("nav");
 
-burger.addEventListener("click", burgerClick);
-function burgerClick() {
+// burger.addEventListener("click", burgerClick);
+// function burgerClick() {
+//   burger.classList.toggle("active");
+//   nav.classList.toggle("active");
+// }
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".main-nav");
+
+burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   nav.classList.toggle("active");
-}
+});
